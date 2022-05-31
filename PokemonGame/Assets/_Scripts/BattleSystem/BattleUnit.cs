b6e0c8657a078c61b310f63c263ced1200184ec0
noxnoctis17 <97007631+noxnoctis17@users.noreply.github.com>
2022-05-31 10:04:43 -0400

    commit from vscode? lol i hate this shit

--- PokemonGame/Assets/_Scripts/BattleSystem/BattleUnit.cs
+++ PokemonGame/Assets/_Scripts/BattleSystem/BattleUnit.cs
@@ -5,35 +5,39 @@ public class BattleUnit : MonoBehaviour
 {
     [SerializeField] private BattleHUD _battleHUD;
     public BattleHUD BattleHUD => _battleHUD;
-    [SerializeField] private OnFieldUnit _onFieldUnit;
+    private OnFieldUnit _onFieldUnit;
     public OnFieldUnit OnFieldUnit => _onFieldUnit;
+    [SerializeField] private GameObject _fieldUnitPrefab;
     [SerializeField] public PokemonSO _pokeSO;
     [SerializeField] private int _level;
     public int Level => _level;
     private BattleAI _battleAI;
     [SerializeField] private bool _isAI;
 
-    public PokemonClass Pokemon {get; set;}
+    public PokemonClass Pokemon { get; set; }
 
     private void Start(){
-        if(_isAI)
+        if( _isAI )
         _battleAI = GetComponent<BattleAI>();
     }
 
-    public void Setup(PokemonClass pokemon){
+    public void Setup( PokemonClass pokemon ){
         _pokeSO = pokemon.PokeSO;
         _level = pokemon.Level;
         Pokemon = pokemon;
         _battleHUD.SetData(Pokemon);
-        // _onFieldUnit.Setup(_pokeSO);
+
+        Instantiate(_fieldUnitPrefab);
+        _onFieldUnit = _fieldUnitPrefab.GetComponent<OnFieldUnit>();
+        _onFieldUnit.Setup(_pokeSO);
     }
 
-    public DamageDetails TakeDamage(MoveClass move, PokemonClass attacker){
+    public DamageDetails TakeDamage( MoveClass move, PokemonClass attacker ){
         var target = Pokemon;
         float critical = 1f;
-        if(UnityEngine.Random.value * 100f <= 6.25f) critical = 1.5f;
+        if( UnityEngine.Random.value * 100f <= 6.25f ) critical = 1.5f;
 
-        float type = TypeChart.GetEffectiveness(move.moveBase.MoveType, target.PokeSO.Type1) * TypeChart.GetEffectiveness(move.moveBase.MoveType, target.PokeSO.Type2);
+        float type = TypeChart.GetEffectiveness( move.moveBase.MoveType, target.PokeSO.Type1 ) * TypeChart.GetEffectiveness( move.moveBase.MoveType, target.PokeSO.Type2 );
         
         var damageDetails = new DamageDetails(){
             TypeEffectiveness = type,
@@ -52,13 +56,14 @@ public class BattleUnit : MonoBehaviour
                 defense = target.SpDefense;
         }
         
-        float random = UnityEngine.Random.Range(0.85f, 1f);
+        float random = UnityEngine.Random.Range( 0.85f, 1f );
 
         float modifiers = random * type * critical;
-        float damageCalc = Mathf.Floor(( 2 * attacker.Level / 5 + 2) * move.moveBase.Power * attack / defense / 50 + 2 ) * modifiers;
-        int damage = (int)damageCalc;
+        float damageCalc = Mathf.Floor( ( 2 * attacker.Level / 5 + 2 ) * move.moveBase.Power * attack / defense / 50 + 2 ) * modifiers;
+        int damage = ( int )damageCalc;
         
-        target.UpdateHP(damage);
+        target.UpdateHP( damage) ;
+        OnFieldUnit.DamageText.text = $"{damage}";
 
         return damageDetails;
     }
--- PokemonGame/Assets/_Scripts/BattleSystem/OnFieldUnit.cs
+++ PokemonGame/Assets/_Scripts/BattleSystem/OnFieldUnit.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Pathfinding;
+using TMPro;
 
 public class OnFieldUnit : MonoBehaviour
 {
-    //i can't believe i lost this whole fucking script lol
+    [SerializeField] private float _searchRadius;
+    [SerializeField] public TextMeshProUGUI DamageText;
+    private AIPath _aiPath;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake(){
+        _aiPath = GetComponent<AIPath>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Setup(PokemonSO pokeSO){
+        _spriteRenderer.sprite = pokeSO.FrontSprite;
+    }
+
+
+    private Vector3 FindPosition(){
+        var point = Random.onUnitSphere * _searchRadius;
+        point.y = 0;
+        point += _aiPath.position;
+        return point;
+    }
 }
